dc00e5e3357f7a30684b7937a648eb421fd260e2
spartechs <2441spartechs@maryknollschool.org>
2026-01-20 17:34:15 -1000

    Remove errors
    so it is able to compile

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -5,7 +5,6 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.SwerveSubsystem;
@@ -16,7 +15,7 @@ public final class Autos
     /** Example static factory for an autonomous command. */
     public static Command exampleAuto(SwerveSubsystem subsystem)
     {
-        return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+        return Commands.sequence(new ExampleCommand(subsystem));
     }
     
     
