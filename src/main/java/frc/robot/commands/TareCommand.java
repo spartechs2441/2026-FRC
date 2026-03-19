5182bf40aca4fcb11f6126d77212026560db60d8
spartechs <2441spartechs@maryknollschool.org>
2026-03-18 15:33:21 -1000

    made a tare command, bindings TBD

---
+++ src/main/java/frc/robot/commands/TareCommand.java
@@ -0,0 +1,64 @@
+// Copyright (c) FIRST and other WPILib contributors.
+
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.SwerveSubsystem;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class TareCommand extends Command {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final SwerveSubsystem subsystem;
+
+
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public TareCommand(SwerveSubsystem subsystem) {
+        this.subsystem = subsystem;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(subsystem);
+
+    }
+
+
+    // Called when the command is initially scheduled.
+    // Run once at beginning
+    @Override
+    public void initialize() {
+        Rotation3d rotation3d = new Rotation3d();
+        subsystem.getSwerveDrive().setGyro(rotation3d);
+
+    }
+
+
+    // Called every time the scheduler runs while the command is scheduled.
+    // Sort of a loop
+    @Override
+    public void execute() {
+
+    }
+
+
+    // Called once the command ends or is interrupted.
+    // Happens at the end
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
